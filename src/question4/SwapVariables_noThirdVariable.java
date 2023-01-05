c5918f008f8963b310243f40eafc2e9a3085feae
michelleschaffer <michellee_e@hotmsil.com>
2023-01-05 11:51:08 -0500

    Added 4th task

---
+++ src/question4/SwapVariables_noThirdVariable.java
@@ -0,0 +1,28 @@
+package question4;
+
+public class SwapVariables_noThirdVariable {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swap: " + a +" "+ b);
+
+        a = a+b;
+        b = a-b;
+        a= a-b;
+
+        System.out.println("Swapped variables: "+ a + " "+ b);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
