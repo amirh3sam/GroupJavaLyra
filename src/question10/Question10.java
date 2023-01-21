b7e2ec4dea411afa1d3e3a979ab6cb5a29fa9edd
desi0610 <desita_mail@yahoo.com>
2023-01-21 15:49:59 -0500

    Question10 solution

--- src/question10/Question10.java
+++ src/question10/Question10.java
@@ -1,5 +1,10 @@
 package question10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Question10 {
 
     /*
@@ -12,4 +17,25 @@ same(" abc ",","cab"); --> true
 same("
 abc abb "); -->
      */
+
+    public static boolean checkIfBuildWithSameLetters(String str1, String str2){
+        char[] arr1 = str1.toCharArray();
+
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        boolean returnResult = true ? Arrays.equals(arr1, arr2):false;
+
+
+        return returnResult;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(checkIfBuildWithSameLetters("abc", "dbac"));
+
+    }
 }
